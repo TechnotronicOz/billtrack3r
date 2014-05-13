af7bd05ff9134bd10fd0fb5287cdb52b21ed363d
Matt Carter <carter.matt@me.com>
2014-05-13 08:55:45 -0500

    an update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.session());
 app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/users.js
+++ routes/users.js
@@ -48,14 +48,21 @@ app.post('/login',
 exports.postlogin = function(req, res, next) {
 	passport.authenticate('local', function(err, user, info) {
 		if (err) { 
+			console.log('err');
 			return next(err);
 		}
 		if (!user) {
+			console.log('!user');
 			//req.session.messages =  [info.message];
 			return res.redirect('/login')
 		}
 		req.logIn(user, function(err) {
-			if (err) { return next(err); }
+			console.log('logIn');
+			if (err) { 
+				console.log('...err');
+				return next(err); 
+			}
+			console.log('logged in');
 			return res.redirect('/');
 		});
 	})(req, res, next);
