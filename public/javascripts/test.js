d57355a7e2fcab0e0328c9e831e208139be4f5c7
Matt Carter <carter.matt@me.com>
2014-05-18 15:31:29 -0500

    Starting to figure Angular out

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,56 +1,40 @@
 var myAppMod = angular.module('myApp', []);
 
-myAppMod.controller('BillListController', function($scope, $http) {
-    $http.get('/bills').success(function(ret) {
-        console.log('ret', ret);
+myAppMod.controller('BillListController', function($scope, $http, $rootScope) {
+    $http.get('/bills').success(function(data) {
+        console.log('data', data);
+        $scope.bills = data;
     });
-    $scope.bills = [
-        {
-            id: 1,
-            name: 'Nebraska Furniture Mart',
-            due_date: new Date('5/18/2014'),
-            amount: 25,
-            pd_date: new Date('5/11/2014'),
-            pd_amt: 250,
-            conf: '4580131'
-        },
-        {
-            id: 2,
-            name: 'Wells Fargo Dealer Services',
-            due_Date: new Date('5/30/2014'),
-            amount: 380,
-            pd_date: null,
-            pd_amt: 0,
-            conf: ''
-        },
-        {
-            id: 3,
-            name: 'KCP&L',
-            due_date: new Date('5/11/2014'),
-            amount: 75,
-            pd_date: new Date('5/9/2014'),
-            pd_amt: 75,
-            conf: '1234'
-        }
-    ];
 
-    $scope.remove = function(index) {
+    $scope.$watch('bills', function(arg1, arg2) {
+        console.log('watch', [arg1, arg2])
+    });
+
+    $rootScope.$on('bill', function(evt, data) {
+        $scope.bills.push(data);
+    })
+
+    /* $scope.remove = function(index) {
         $scope.bills.splice(index, 1);
-    }
+    } */
 });
 
-myAppMod.controller('NewBillCtrl', function($scope, $http) {
+myAppMod.controller('NewBillCtrl', function($scope, $http, $rootScope) {
+    $scope.formState = false;
+
+    $scope.toggleForm = function() {
+        console.log('toggleForm');
+        $scope.formState = !$scope.formState;
+    }
+
     $scope.SaveData = function() {
         console.log($scope);
+
         $http.post('/bills', $scope.bill).success(function(data) {
+            $rootScope.$emit('bill', data)
             console.log('success', data);
         }).error(function(data) {
             console.log('error', data);
         });
     };
-    /* $http.post('/bills').success(function(data) {
-        console.log('success', data);
-    }).error(function(data) {
-        console.log('error', data);
-    }); */
 });
