426e9c890396b7dbd4e63b3f319475ae7f5df5a7
Matt Carter <carter.matt@me.com>
2014-05-18 21:28:27 -0500

    adding angular filtering

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,25 +1,19 @@
 var myAppMod = angular.module('myApp', []);
 
-myAppMod.controller('BillListController', function($scope, $http, $rootScope) {
+myAppMod.controller('BillListCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
     $http.get('/bills').success(function(data) {
-        console.log('data', data);
         $scope.bills = data;
     });
 
-    $scope.$watch('bills', function(arg1, arg2) {
-        console.log('watch', [arg1, arg2])
-    });
+    $scope.$watch('bills', function(oldData, newData, scope) { });
 
     $rootScope.$on('bill', function(evt, data) {
         $scope.bills.push(data);
-    })
+    });
+}]);
 
-    /* $scope.remove = function(index) {
-        $scope.bills.splice(index, 1);
-    } */
-});
 
-myAppMod.controller('NewBillCtrl', function($scope, $http, $rootScope) {
+myAppMod.controller('NewBillCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
     $scope.formState = false;
 
     $scope.toggleForm = function() {
@@ -31,10 +25,10 @@ myAppMod.controller('NewBillCtrl', function($scope, $http, $rootScope) {
         console.log($scope);
 
         $http.post('/bills', $scope.bill).success(function(data) {
-            $rootScope.$emit('bill', data)
-            console.log('success', data);
+            $rootScope.$emit('bill', data);
+            $scope.formState = false;
         }).error(function(data) {
             console.log('error', data);
         });
     };
-});
+}]);
